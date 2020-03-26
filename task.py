21ce7f5d6116f52fbbbb65cc75c12ceda7e6d17d
Sodiq Akintunde <damilarey98@gmail.com>
2020-03-26 11:36:50 +0100

    new file added

---
+++ task.py
@@ -0,0 +1,5 @@
+from math import pi
+print("Area of circle is pi*r**2")
+radius = float(input("Enter your radius: "))
+area_of_circle = pi * (radius**2)
+print("Area of circle with radius %d is given as %.2f" %(radius, area_of_circle))
